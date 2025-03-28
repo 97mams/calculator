434b2445570d958ca5b0cee36399103304657e46
97mams <97mams@gmail.com>
2025-03-28 13:57:33 -0700

    changed if statement to switch

--- index.php
+++ index.php
@@ -9,19 +9,24 @@ $operator = (int)readline("choisir un operatuer:(tapez 1 pour +, tapez 2 pour -,
 $numberTwo = (float)readline("entrer séconde nombre: ");
 $calcul = new Calculator($numberOne, $numberTwo);
 
+switch ($operator) {
+    case 1:
+        echo $calcul->addition()."\n";
+        break;
+    
+    case 2:
+        echo $calcul->substraction()."\n";
+        break;
 
-if ($operator === 1)
-{
-    echo $calcul->addition()."\n";
-} else if ($operator === 2)
-{
-    echo $calcul->substraction()."\n";
-} else if ($operator === 3)
-{
-    echo $calcul->multiplication()."\n";
-} else if ($operator === 4)
-{
-    echo $calcul->division()."\n";
-} else {
-    echo "veuillez choisir un operatuer\n";
+    case 3:
+        echo $calcul->multiplication()."\n";
+        break;
+
+    case 4:
+        echo $calcul->substraction()."\n";
+        break;
+
+    default:
+        echo "Veuillez choisir un operatuer\n";
+        break;
 }
